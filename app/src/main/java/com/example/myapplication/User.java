f72e0c4c32bfc08a81a9dd6f6febb57f6be6e692
Ayesha Siddiqua <ayesha@linkstaff.co.jp>
2019-10-11 11:34:57 +0600

    listview+arrayadapter

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class User {
+    public String name;
+    public String hometown;
+
+    public User(String name, String hometown) {
+        this.name = name;
+        this.hometown = hometown;
+    }
+}
